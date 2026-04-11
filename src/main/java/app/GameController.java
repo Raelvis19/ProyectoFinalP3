6918a0da5f35a5e4974aed687d76e3ffa0dd2836
Raelvis Francisco Paulino Almanzar <raelvis_paulino@ucne.edu.do>
2026-04-11 08:46:21 -0400

    tuve que crear los enemgigos y para eso hice 10 modelos diferentes en Photoshop agreando 3 imagenes por cada modelo para simular movimiento y cambio ademas de que descubri que para poder eliminar un enemigo cada ufo deber ser un objeto independiente para que se pueda detectar una colision

--- src/main/java/app/GameController.java
+++ src/main/java/app/GameController.java
@@ -1,60 +1,237 @@
 package main.java.app;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 public class GameController {
+
     @FXML
     private ImageView nave;
+
+    @FXML
+    private AnchorPane gamePane;
+
+    @FXML
+    private HBox vidasBox;
 
     private boolean moveLeft = false;
     private boolean moveRight = false;
 
     private double velocidad = 5;
 
+
+    private ImageView[][] enemigos;
+
+    
+    private Image[][] modelos;
+
+    private int filas = 3;
+    private int columnas = 6;
+
+    private boolean moverDerecha = true;
+    private double velocidadUfo = 1;
+
     @FXML
     public void initialize() {
 
-    // Esperar a que la escena exista
-    nave.sceneProperty().addListener((obs, oldScene, newScene) -> {
-        if (newScene != null) {
+        
+        modelos = new Image[][]{
+            {
+                new Image(getClass().getResource("/main/resources/img/gg1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/gg2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/gg3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/nm1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/nm2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/nm3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pulA1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pulA2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pulA3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pm1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pm2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pm3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pa1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pa2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pa3.png").toExternalForm())
+            },
+            {
+                new Image(getClass().getResource("/main/resources/img/azul1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/azul2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/azul3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/ojoM1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/ojoM2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/ojoM3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pp1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pp2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/pp3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/v1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/v2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/v3.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/nVerde1.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/nVerde2.png").toExternalForm()),
+                new Image(getClass().getResource("/main/resources/img/nVerde3.png").toExternalForm())
+            }
+        };
 
-            newScene.setOnKeyPressed(event -> {
-                switch (event.getCode()) {
-                    case A -> moveLeft = true;
-                    case D -> moveRight = true;
-                }
-            });
+        crearFormacion();
 
-            newScene.setOnKeyReleased(event -> {
-                switch (event.getCode()) {
-                    case A -> moveLeft = false;
-                    case D -> moveRight = false;
-                }
-            });
-        }
-    });
+        
+        nave.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
 
-    iniciarMovimiento();
+                newScene.setOnKeyPressed(event -> {
+                    switch (event.getCode()) {
+                        case A -> moveLeft = true;
+                        case D -> moveRight = true;
+                    }
+                });
+
+                newScene.setOnKeyReleased(event -> {
+                    switch (event.getCode()) {
+                        case A -> moveLeft = false;
+                        case D -> moveRight = false;
+                    }
+                });
+
+                nave.requestFocus();
+            }
+        });
+
+        iniciarMovimiento();
+        moverFormacion();
+        animarFormacion();
     }
 
+    
     private void iniciarMovimiento() {
 
         new AnimationTimer() {
-        @Override
-        public void handle(long now) {
+            @Override
+            public void handle(long now) {
 
-            double x = nave.getTranslateX();
+                double x = nave.getTranslateX();
 
-            if (moveLeft) {
-                nave.setTranslateX(x - velocidad);
+                if (moveLeft) {
+                    nave.setTranslateX(x - velocidad);
+                }
+
+                if (moveRight) {
+                    nave.setTranslateX(x + velocidad);
+                }
             }
+        }.start();
+    }
 
-            if (moveRight) {
-                nave.setTranslateX(x + velocidad);
+
+    private void crearFormacion() {
+
+        enemigos = new ImageView[filas][columnas];
+
+        for (int f = 0; f < filas; f++) {
+            for (int c = 0; c < columnas; c++) {
+
+                int tipo = f % modelos.length; 
+
+                ImageView enemigo = new ImageView(modelos[tipo][0]);
+
+                enemigo.setFitWidth(100);
+                enemigo.setPreserveRatio(true);
+
+                enemigo.setLayoutX(100 + c * 120);
+                enemigo.setLayoutY(250 + f * 90);
+
+                enemigos[f][c] = enemigo;
+                gamePane.getChildren().add(enemigo);
             }
         }
+    }
+
+    
+    private void moverFormacion() {
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+
+                boolean cambiarDireccion = false;
+
+                for (int f = 0; f < filas; f++) {
+                    for (int c = 0; c < columnas; c++) {
+
+                        ImageView enemigo = enemigos[f][c];
+                        if (enemigo == null) continue;
+
+                        double x = enemigo.getLayoutX();
+
+                        if (moverDerecha) {
+                            enemigo.setLayoutX(x + velocidadUfo);
+
+                            if (x >= gamePane.getWidth() - enemigo.getFitWidth()) {
+                                cambiarDireccion = true;
+                            }
+
+                        } else {
+                            enemigo.setLayoutX(x - velocidadUfo);
+
+                            if (x <= 0) {
+                                cambiarDireccion = true;
+                            }
+                        }
+                    }
+                }
+
+                
+                if (cambiarDireccion) {
+                    moverDerecha = !moverDerecha;
+
+                    for (int f = 0; f < filas; f++) {
+                        for (int c = 0; c < columnas; c++) {
+
+                            ImageView enemigo = enemigos[f][c];
+                            if (enemigo != null) {
+                                enemigo.setLayoutY(enemigo.getLayoutY() + 15);
+                            }
+                        }
+                    }
+                }
+            }
         }.start();
+    }
+
+    
+    private void animarFormacion() {
+
+        Timeline animacion = new Timeline(
+            new KeyFrame(Duration.millis(500), e -> {
+
+                for (int f = 0; f < filas; f++) {
+                    for (int c = 0; c < columnas; c++) {
+
+                        ImageView enemigo = enemigos[f][c];
+                        if (enemigo == null) continue;
+
+                        int tipo = f % modelos.length;
+
+                        int frame = (enemigo.getUserData() != null)
+                                ? (int) enemigo.getUserData()
+                                : 0;
+
+                        int siguiente = (frame + 1) % modelos[tipo].length;
+
+                        enemigo.setImage(modelos[tipo][siguiente]);
+                        enemigo.setUserData(siguiente);
+                    }
+                }
+            })
+        );
+
+        animacion.setCycleCount(Timeline.INDEFINITE);
+        animacion.play();
     }
 }
